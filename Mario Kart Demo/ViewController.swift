f6d838f8adf278ee8b2674038d2ab1aabe2b584d
Subba <Subba@gmail.com>
2021-03-06 13:02:16 -0600

    Initial Commit

---
+++ Mario Kart Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mario Kart Demo
+//
+//  Created by SubbaLakshmi on 3/6/21.
+//  Copyright © 2021 SubbaLakshmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
